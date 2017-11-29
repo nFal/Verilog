f976ea9373ce7fece2000d90840e1e1595948fc5
noufal <noupabme@gmail.com>
2017-11-29 23:11:17 +0530

    changed code to model 6116 sram ic

--- subleq-mach/RAM.v
+++ subleq-mach/RAM.v
@@ -1,23 +1,23 @@
 module RAM (
-             input        ctl,  //0 = write; 1 = read
-             input        ena,  //1 = enable; 0 = disable
+             input        ope,  //0 = output enable; 1 = disable
+	     input        ctl,  //0 = write; 1 = read
+             input        ena,  //0 = enable; 1 = disable
              input  [7:0] adr,
              inout  [7:0] dat
 );
 
 
 reg [7:0] mem[0:255];
-reg [7:0] dt;
 
-always @(adr or ctl or ena) begin
-  if (ena==1) begin
-    if (ctl)
-      dt=mem[adr];
-    else 
-      mem[adr]=dat;
+assign dat = (ope==1'b1 | ctl==1'b0 | ena==1'b1) ? 
+             8'bZZZZZZZZ : mem[adr];
+
+always @(ctl,ena) begin
+  @(negedge ctl) begin
+    if (ena==1'b0) begin
+      mem[adr] <= dat;
+    end
   end
 end
-
-assign dat = (ctl==1) ? dt : 8'hZZ;
 
 endmodule
