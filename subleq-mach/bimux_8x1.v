7c3346703c418deaa3886bb836d2f10cc2b799cf
Noufal <noupabme@gmail.com>
2017-11-29 00:40:18 -0800

    rectified an error in dealing inout

--- subleq-mach/bimux_8x1.v
+++ subleq-mach/bimux_8x1.v
@@ -5,31 +5,36 @@ module bimux_8x1 (
                    inout       out
 );
 
+reg out1;
+reg [7:0] inp1;
+
 always @(sel or dir) begin
   if (dir==0) begin
     case (sel)
-      0: out = inp[0];
-      1: out = inp[1];
-      2: out = inp[2];
-      3: out = inp[3];
-      4: out = inp[4];
-      5: out = inp[5];
-      6: out = inp[6];
-      7: out = inp[7];
+      0: out1 = inp[0];
+      1: out1 = inp[1];
+      2: out1 = inp[2];
+      3: out1 = inp[3];
+      4: out1 = inp[4];
+      5: out1 = inp[5];
+      6: out1 = inp[6];
+      7: out1 = inp[7];
     endcase
   end
   else if (dir==1) begin
     case (sel)
-      0: inp[0] = out;
-      1: inp[1] = out;
-      2: inp[2] = out;
-      3: inp[3] = out;
-      4: inp[4] = out;
-      5: inp[5] = out;
-      6: inp[6] = out;
-      7: inp[7] = out;
+      0: inp1[0] = out;
+      1: inp1[1] = out;
+      2: inp1[2] = out;
+      3: inp1[3] = out;
+      4: inp1[4] = out;
+      5: inp1[5] = out;
+      6: inp1[6] = out;
+      7: inp1[7] = out;
     endcase
   end
 end
+assign out=out1;
+assign inp=inp1;
 
 endmodule
